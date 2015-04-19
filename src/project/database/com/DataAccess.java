41c6bfd1fb276575f7fd3f0d648511788dca465c
Umar Majeed <umarmajeed@ufl.edu>
2015-04-19 14:33:27 -0400

    updated Api's
    Added code for top ten, game player stats and comparison

--- src/project/database/com/DataAccess.java
+++ src/project/database/com/DataAccess.java
@@ -219,4 +219,211 @@ public class DataAccess {
 		return returnString;
 	
 	}
+	@Path("/topten")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String ReturnTopTen(@QueryParam("season") String season,@QueryParam("section") String section) throws Exception {
+		String returnString = null;
+		Connection conn = null;
+		
+		//System.out.println("team:" + team);
+		
+		try{
+			conn = OracleConnection.OracleConnectionConn().getConnection();
+			
+			//System.out.println("team: "+team);
+			//System.out.println("season: "+season);
+			String getteams = "{call PKG_PLAYERDATA.Get_Top_Ten(?,?,?)}";
+			CallableStatement callableStatement = conn.prepareCall(getteams);
+			callableStatement.setString(1, season);
+			callableStatement.setString(2, section);
+			callableStatement.registerOutParameter(3, OracleTypes.CURSOR);
+			
+			callableStatement.executeUpdate();
+			
+			ResultSet rs = (ResultSet)callableStatement.getObject(3);
+			
+			ToJSON converter = new ToJSON();
+			JSONArray json = new JSONArray();
+			
+			json = converter.toJSONArray(rs);
+			
+			returnString = json.toString();
+		}
+		catch(Exception ex){
+			ex.printStackTrace();
+		}
+		finally{
+			if (conn != null) conn.close();
+			
+		}
+		return returnString;
+	
+	}
+	@Path("/game")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String ReturnGame(@QueryParam("date") String date) throws Exception {
+		String returnString = null;
+		Connection conn = null;
+		
+		//System.out.println("team:" + team);
+		
+		try{
+			conn = OracleConnection.OracleConnectionConn().getConnection();
+			
+			//System.out.println("team: "+team);
+			//System.out.println("season: "+season);
+			String getteams = "{call PKG_GAME.Get_Game(?,?)}";
+			CallableStatement callableStatement = conn.prepareCall(getteams);
+			callableStatement.setString(1, date);
+			callableStatement.registerOutParameter(2, OracleTypes.CURSOR);
+			
+			callableStatement.executeUpdate();
+			
+			ResultSet rs = (ResultSet)callableStatement.getObject(2);
+			
+			ToJSON converter = new ToJSON();
+			JSONArray json = new JSONArray();
+			
+			json = converter.toJSONArray(rs);
+			
+			returnString = json.toString();
+		}
+		catch(Exception ex){
+			ex.printStackTrace();
+		}
+		finally{
+			if (conn != null) conn.close();
+			
+		}
+		return returnString;
+	
+	}
+	
+	@Path("/gameTeamStats")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String ReturnGameTeamStats(@QueryParam("gameId") String game,@QueryParam("team") String team) throws Exception {
+		String returnString = null;
+		Connection conn = null;
+		
+		//System.out.println("team:" + team);
+		
+		try{
+			conn = OracleConnection.OracleConnectionConn().getConnection();
+			
+			//System.out.println("team: "+team);
+			//System.out.println("season: "+season);
+			String getteams = "{call PKG_GAME.Get_Game_TeamStats(?,?,?)}";
+			CallableStatement callableStatement = conn.prepareCall(getteams);
+			callableStatement.setString(1, game);
+			callableStatement.setString(2, team);
+			callableStatement.registerOutParameter(3, OracleTypes.CURSOR);
+			
+			callableStatement.executeUpdate();
+			
+			ResultSet rs = (ResultSet)callableStatement.getObject(3);
+			
+			ToJSON converter = new ToJSON();
+			JSONArray json = new JSONArray();
+			
+			json = converter.toJSONArray(rs);
+			
+			returnString = json.toString();
+		}
+		catch(Exception ex){
+			ex.printStackTrace();
+		}
+		finally{
+			if (conn != null) conn.close();
+			
+		}
+		return returnString;
+	
+	}
+	
+	@Path("/gamePlayerStats")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String ReturnGameplayerStats(@QueryParam("gameId") String game,@QueryParam("team") String team) throws Exception {
+		String returnString = null;
+		Connection conn = null;
+		
+		//System.out.println("team:" + team);
+		
+		try{
+			conn = OracleConnection.OracleConnectionConn().getConnection();
+			
+			//System.out.println("team: "+team);
+			//System.out.println("season: "+season);
+			String getteams = "{call PKG_GAME.Get_Game_PlayerStats(?,?,?)}";
+			CallableStatement callableStatement = conn.prepareCall(getteams);
+			callableStatement.setString(1, game);
+			callableStatement.setString(2, team);
+			callableStatement.registerOutParameter(3, OracleTypes.CURSOR);
+			
+			callableStatement.executeUpdate();
+			
+			ResultSet rs = (ResultSet)callableStatement.getObject(3);
+			
+			ToJSON converter = new ToJSON();
+			JSONArray json = new JSONArray();
+			
+			json = converter.toJSONArray(rs);
+			
+			returnString = json.toString();
+		}
+		catch(Exception ex){
+			ex.printStackTrace();
+		}
+		finally{
+			if (conn != null) conn.close();
+			
+		}
+		return returnString;
+	
+	}
+	
+	@Path("/compare")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String Compare(@QueryParam("playerOne") String playerOne,@QueryParam("playerTwo") String playerTwo) throws Exception {
+		String returnString = null;
+		Connection conn = null;
+		
+		//System.out.println("team:" + team);
+		
+		try{
+			conn = OracleConnection.OracleConnectionConn().getConnection();
+			
+			System.out.println(playerOne);
+			System.out.println(playerTwo);
+			String comparePlayers = "{call PKG_PLAYERDATA.Compare(?,?,?)}";
+			CallableStatement callableStatement = conn.prepareCall(comparePlayers);
+			callableStatement.setString(1, playerOne);
+			callableStatement.setString(2, playerTwo);
+			callableStatement.registerOutParameter(3, OracleTypes.CURSOR);
+			
+			callableStatement.executeUpdate();
+			
+			ResultSet rs = (ResultSet)callableStatement.getObject(3);
+			
+			ToJSON converter = new ToJSON();
+			JSONArray json = new JSONArray();
+			
+			json = converter.toJSONArray(rs);
+			
+			returnString = json.toString();
+		}
+		catch(Exception ex){
+			ex.printStackTrace();
+		}
+		finally{
+			if (conn != null) conn.close();
+			
+		}
+		return returnString;
+	
+	}
 }
